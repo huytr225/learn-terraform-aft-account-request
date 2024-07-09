6f8cf95395a6a72fd81ef7ac7463364c8546de10
HuyTD <huytr225@gmail.com>
2024-07-09 11:06:36 +0700

    update

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,7 @@
 output "sandbox2_account_id" {
-    value = module.sandbox2.account_id
+    value = module.sandbox2.aws_organizations_organization_account.account
 }
 
 output "sandbox1_account_id" {
-    value = module.sandbox.account_id
+    value = module.sandbox.aws_organizations_organization_account.account
 }
--- terraform/sandbox-account-2.tf
+++ terraform/sandbox-account-2.tf
@@ -1,22 +1,22 @@
-module "sandbox" {
+module "sandbox2" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "huytd.test3.aws@outlook.com"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Old (ou-ffdz-dcvv5s4f)"
+    AccountEmail              = "huytd.test5.aws@outlook.com"
+    AccountName               = "sandbox-aft-2"
+    ManagedOrganizationalUnit = "Labs (ou-ffdz-hw8f5zby)"
     SSOUserEmail              = "huytd.aft.aws@outlook.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT update"
+    "Learn Tutorial" = "AFT"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn update"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform update"
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
 
   custom_fields = {
