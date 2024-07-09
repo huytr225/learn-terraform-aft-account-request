65a1cd912bd29a7405f26525720556a209263638
HuyTD <huytr225@gmail.com>
2024-07-09 11:11:41 +0700

    update

--- terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "sandbox2_account_id" {
-    value = module.sandbox2.aws_organizations_organization_account.account
-}
-
-output "sandbox1_account_id" {
-    value = module.sandbox.aws_organizations_organization_account.account
-}
