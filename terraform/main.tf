ff9c84ba15dbc0b042ea67343a97799362d5f19e
HuyTD <huytr225@gmail.com>
2024-07-10 14:33:02 +0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,30 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
+# module "sandbox" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "huytd.test3.aws@outlook.com"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Infrastructure (ou-ffdz-lqp0nwb1)"
-    SSOUserEmail              = "huytd.aft.aws@outlook.com"
-    SSOUserFirstName          = "Sandbox1"
-    SSOUserLastName           = "AFT"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "huytd.test3.aws@outlook.com"
+#     AccountName               = "sandbox-aft"
+#     ManagedOrganizationalUnit = "Infrastructure (ou-ffdz-lqp0nwb1)"
+#     SSOUserEmail              = "huytd.aft.aws@outlook.com"
+#     SSOUserFirstName          = "Sandbox1"
+#     SSOUserLastName           = "AFT"
+#   }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT update1"
-  }
+#   account_tags = {
+#     "Learn Tutorial" = "AFT update1"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn update1"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform update1"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn update1"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform update1"
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#   }
 
-  account_customizations_name = "sandbox"
-}
+#   account_customizations_name = "sandbox"
+# }
 
 module "sandbox2" {
   source = "./modules/aft-account-request"
