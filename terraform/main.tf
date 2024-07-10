534babb5662685cffd9dae612167674bd708341e
HuyTD <huytr225@gmail.com>
2024-07-10 14:47:06 +0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "sandbox" {
   control_tower_parameters = {
     AccountEmail              = "huytd.test3.aws@outlook.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Sandbox (ou-ffdz-zsxcbzkw)"
+    ManagedOrganizationalUnit = "Infrastructure (ou-ffdz-lqp0nwb1)"
     SSOUserEmail              = "huytd.aft.aws@outlook.com"
     SSOUserFirstName          = "Sandbox1"
     SSOUserLastName           = "AFT"
@@ -12,6 +12,14 @@ module "sandbox" {
 
   account_tags = {
     "Learn Tutorial" = "AFT update1"
+    "ABC:Owner"       = "myname@mycompany.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
   }
 
   change_management_parameters = {
@@ -21,7 +29,10 @@ module "sandbox" {
 
   custom_fields = {
     group = "non-prod"
+    custom1 = "a"
+    custom2 = "b"
   }
+  
 
   account_customizations_name = "sandbox"
 }
